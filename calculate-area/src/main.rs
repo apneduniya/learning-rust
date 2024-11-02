48bcd09c8f99dded4518ba28660140792dfdd359
apneduniya <apneduniya.dontdisturb@gmail.com>
2024-11-03 05:12:03 +0530

    update: Replaced function with impl in calculate area program

--- calculate-area/src/main.rs
+++ calculate-area/src/main.rs
@@ -6,18 +6,49 @@ struct Rectangle {
     width: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    fn is_bigger(&self, other_rect: &Rectangle) -> bool {
+        self.area() > other_rect.area()
+    }
+}
+
+impl Rectangle {
+    fn get_height(&self) -> u32 {
+        self.height
+    }
+
+    fn get_width(&self) -> u32 {
+        self.width
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
+
 fn main() {
     let rect1 = Rectangle{
         height: 32,
         width: 46
     };
 
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-}
+    let rect2 = Rectangle {
+        height: 45,
+        width: 10
+    };
 
+    println!("The area of the rect1 is {} square pixels.", rect1.area());
+    println!("The area of the rect2 is {} square pixels.\n", rect2.area());
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.height * rect.width
+    println!("The dimensions of the rect1 is {}x{} pixels.", rect1.get_height(), rect1.get_width());
+    if rect1.width() { println!("Is width of the rect1 non-zero? {}\n", rect1.width()); }
+
+    println!("Is rect1 bigger than rect2? {}", rect1.is_bigger(&rect2))
 }
 
 
